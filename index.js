73c1102e30f82a8f683e2e5fe38a0c9462d643bb
Djabrail <dsadulaev@mail.ru>
2022-08-29 23:07:45 +0300

    первый коммит со скилетом бекэнда

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+app.use(cors());
+app.use(express.static("public"));
+app.use(require("./routes/book.routes"));
+app.use(require("./routes/genre.routes"));
+app.use(require("./routes/user.routes"));
+
+mongoose.connect(
+  "mongodb+srv://Djabrail:4815162342@cluster0.wkvhjdw.mongodb.net/library",
+  () => {
+    app.listen(process.env.PORT, () => {
+      console.log("worked and connected");
+    });
+  }
+);
---
+++ routes/book.routes.js
@@ -0,0 +1,7 @@
+const { Router } = require("express");
+const router = Router();
+
+//
+// тут должны быть ваши роуты
+
+module.exports = router;
