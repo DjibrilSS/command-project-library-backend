e86dddc7eddf18fb2b2a196de3f83b553764976c
Isa <tepsurkaev99999@mail.ru>
2022-08-30 11:25:58 +0300

    сделал жанры

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 app.use(cors());
+app.use(express.json());
 app.use(express.static("public"));
 app.use(require("./routes/book.routes"));
 app.use(require("./routes/genre.routes"));
@@ -12,7 +13,7 @@ app.use(require("./routes/user.routes"));
 mongoose.connect(
   "mongodb+srv://Djabrail:4815162342@cluster0.wkvhjdw.mongodb.net/library",
   () => {
-    app.listen(process.env.PORT, () => {
+    app.listen(4000, () => {
       console.log("worked and connected");
     });
   }
