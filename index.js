dd1bb7538f920905b940d2ce9f5d2ed50c4867ea
OdinTakoy <bugazov9@mail.ru>
2022-08-31 15:47:31 +0300

    Сделал возможность добавления аватарки

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require("path")
 const cors = require("cors");
 require("dotenv").config();
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
 app.use(require("./routes/book.routes"));
 app.use(require("./routes/genre.routes"));
+app.use(express.static(path.resolve(__dirname, "public")));
+app.use("/public",express.static(path.resolve(__dirname, "public")));
 app.use(require("./routes/user.routes"));
+
+app.use("/api",require("./routes/upload.route"))
 
 mongoose.connect(
   "mongodb+srv://Djabrail:4815162342@cluster0.wkvhjdw.mongodb.net/library",
